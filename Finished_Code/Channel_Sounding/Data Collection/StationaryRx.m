0c88f41e558cd053b7cc54a8de301e3512fce173
daktal7 <dakotaflanary@gmail.com>
2018-08-03 10:13:34 -0600

    added a missing ;

--- Finished_Code/Channel_Sounding/Data Collection/StationaryRx.m
+++ Finished_Code/Channel_Sounding/Data Collection/StationaryRx.m
@@ -172,7 +172,7 @@ for runs = 1:NumSamples
     eval(sprintf('load("StationaryData/%s_pwelch_%d.mat");',room,runs));
     eval(sprintf('signal = %s_pwelch_%d(1:2:end,1);',room,runs));
     eval(sprintf('noise = %s_pwelch_%d(2:2:end,1);',room,runs));
-    difference = abs(signal(:,1) - noise(:,1))
+    difference = abs(signal(:,1) - noise(:,1));
     count = 0;
     for carriers = 1:64
         if difference(carriers,1) >= db
