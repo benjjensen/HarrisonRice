78c63c773d9ddd786a64af512864f4ffae6a39c5
Nathan Nelson <nathan.nelson.97@gmail.com>
2018-08-07 13:34:05 -0600

    Added more timing to acquire.cpp

--- Finished_Code/V2V/matlab/simpleReadData.m
+++ Finished_Code/V2V/matlab/simpleReadData.m
@@ -1,14 +1,14 @@
 % clear all
 
-data = readBatched_DAQshortSamples_host('../acquire/uvdma.dat');
+data = readBatched_DAQshortSamples_host('/media/V2V/1_block_new_2018-08-06__15-59-30.dat');
 
 figure(1); clf
-plot(1:40,data(1:40)-9260.7,'.-',...
-    1:40,data(41:80)-9260.7,'-.',...
-    1:40,data(81:120)-9260.7,'.-'); grid on;
+plot(1:300,data(1:300)-8180.7,'.-'); grid on;%,...
+    %1:300,data(301:600)-8180.7,'-.',...
+    %1:300,data(601:900)-8180.7,'.-'); grid on;
 
 Nfft = 1000;
-PP = pwelch(data(1:500000)-9260.7,ones(1,Nfft),0,Nfft,'twosided');
+PP = pwelch(data(1:500000)-8180.7,ones(1,Nfft),0,Nfft,'twosided');
 FF = -0.5:1/Nfft:0.5-1/Nfft;
 figure(2);
 plot(FF*100,10*log10(fftshift(PP))); grid on;
