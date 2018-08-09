86e0ed9e9bd89a6b900ec588d9076192a3d1b428
Nathan Nelson <nathan.nelson.97@gmail.com>
2018-08-09 14:07:52 -0600

    Added analyze_harrison_advantage

---
+++ Practice_Files/Nathan/codeInfo.m
@@ -0,0 +1,19 @@
+classdef codeInfo
+    properties
+        u
+        m
+        % block length (bits)
+        n
+        % message length (bits)
+        k
+        rate
+        dBLevel
+        % number of received bits
+        mu
+        % equivocation (bits)
+        H
+        percentLeaked
+        carrierRate
+    end
+end
+
