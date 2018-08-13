a8df799e125a1ae163d822d61de5ccf6eb1817cf
daktal7 <dakotaflanary@gmail.com>
2018-08-13 15:54:00 -0600

    added comments

--- Finished_Code/Information_Theory/Mu_Distribution/bl_get_dist.m
+++ Finished_Code/Information_Theory/Mu_Distribution/bl_get_dist.m
@@ -1,6 +1,11 @@
 function [pr_dist] = bl_get_dist(pr_mat)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+%bl_get_dist gets the distribution of all of the combos and averages the
+%probabilities to get the average distribution
+%   input is a matrix of the possible combinations with each row being a
+%   different combination of carriers, each combination is passed into the get_values
+%   function where the probability for each number of revealed bits is
+%   calculated, if there is more than one combination the probabilities are
+%   averaged.
 [num_rows,n] = size(pr_mat);
 probabilities = zeros(num_rows,(n+1));
 for row = 1:num_rows
