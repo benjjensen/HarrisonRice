be457166d4bc4026e80030c4fe86d077b4087c76
kalin <kalinnorman@gmail.com>
2018-12-27 07:29:42 -0700

    Included secrecy capacity

--- Practice_Files/Kalin/QPSK/QPSK_Capacity.m
+++ Practice_Files/Kalin/QPSK/QPSK_Capacity.m
@@ -19,23 +19,15 @@ harrison_max = max(max(capacity(36:65,65:98)));
 [X Y] = find(capacity(36:65,65:98) == harrison_max);
 max_X = X + 36 - 1;
 max_Y = Y + 65 - 1;
-%% Secrecy Capacity
 
-% This section is currently incomplete. The code below is copied over from
-% the previous capacity file and needs to be updated to match how secrecy
-% capacity will be calculated for QPSK and our data now. 
+%% Secrecy Capacity 
 
-% for x = 1:90
-%     for y = 1:345
-%         tx2_secrecy_capacity(x,y) = harrison_max_capacity - tx2_capacity(x,y);
-%         tx2_adjusted_secrecy_capacity(x,y) = tx2_adjusted_harrison_max - tx2_adjusted_capacity(x,y);
-%         if (tx2_secrecy_capacity(x,y) < 0)
-%             tx2_secrecy_capacity(x,y) = 0;
-%         end
-%         if (tx2_adjusted_secrecy_capacity(x,y) < 0)
-%             tx2_adjusted_secrecy_capacity(x,y) = 0;
-%         end
-%     end
-% end
-% save('tx2_secrecy_capacity.mat','tx2_secrecy_capacity');
-% save('tx2_adjusted_secrecy_capacity.mat','tx2_adjusted_secrecy_capacity');
+for x = 1:90
+    for y = 1:345
+        secrecy_capacity(x,y) = harrison_max - capacity(x,y);
+        if (secrecy_capacity(x,y) < 0)
+            secrecy_capacity(x,y) = 0;
+        end
+    end
+end
+save('secrecy_capacity.mat','secrecy_capacity');
