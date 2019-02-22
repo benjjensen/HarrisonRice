6b14224f9c50ebad16b6d951a6283dc313472de1
Nathan Nelson <nathan.nelson.97@gmail.com>
2019-02-22 16:03:14 -0700

    Changed acquire a bit

---
+++ Finished_Code/V2V/matlab/channel_sounder_single_tone.m
@@ -0,0 +1,11 @@
+%%%% Lindsey's channel sounding signal
+
+N0 = 320;
+
+x = ones(N0, 1);
+
+% x = (0:2*pi/N0:2*pi*((N0-1)/N0));
+% x = sin(x)';
+
+% figure();
+% plot(x);
---
+++ Finished_Code/V2V/matlab/nathan_channel_sounder_src.m
@@ -0,0 +1,3 @@
+steps = 300;
+t = (0:2*pi/(steps-1):2*pi);
+x = sin(t)';
