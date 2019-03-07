4ef5afad5d2b1dde8d129b35a6a5d6cde589508c
Bradford Clark <bradford96789@gmail.com>
2019-03-06 22:12:57 -0700

    got the patch thing working

--- CurrentProjects/V2VSecurity/UnderConstruction/codedV2I.m
+++ CurrentProjects/V2VSecurity/UnderConstruction/codedV2I.m
@@ -1,5 +1,9 @@
+%Created by Bradford Clark
+% March 6, 2019
 % close all
-addpath('../Functions')
+addpath('../Functions');
+addpath('../Data');
+load('BradfordShouldPayAttention.mat');
 number = 32;
 for i = 1:number
     if i == 31
@@ -8,9 +12,10 @@ for i = 1:number
         fakeData(1,i) = mod(i,33);
     end
 end
+maxNumberOfCarriers = 32;
+secrecyCapacityPerLocation = floor(capacity(1:10:length(capacity),1)' * maxNumberOfCarriers);
 secrecyCapacityPerLocation = fakeData;
 numberOfSpots = length(secrecyCapacityPerLocation);
-maxNumberOfCarriers = 32;
 r = 2;
 m = 5;
 n = 16;
@@ -19,23 +24,22 @@ RMWeights = RMWeightHier(r,m,false);
 bobsKnowledge = zeros(numberOfSpots);
 
 for i = 1:numberOfSpots
-    if secrecyCapacityPerLocation(i) == maxNumberOfCarriers
-        bobsKnowledge(:,i) = maxNumberOfCarriers;
+    if secrecyCapacityPerLocation(i) == 32
+        bobsKnowledge(:,i) = 0;
     else
         bobsKnowledge(:,i) = NaN;
     end
 end
-
 figure(100);
 hold on;
-bob = surf(flipud(bobsKnowledge)-maxNumberOfCarriers);
-grid off;
-% cData = [1 1 1];
-caxis([0 32]);
-view(0,90);
-myColorMap = ones(128,3); % Set row 128 to white.
-% Then apply the colormap
-colormap(myColorMap);
+% bob = surf(flipud(bobsKnowledge));%-maxNumberOfCarriers);
+% grid off;
+% % cData = [1 1 1];
+% caxis([0 32]);
+% view(0,90);
+% myColorMap = ones(128,3); % Set row 128 to white.
+% % Then apply the colormap
+% colormap(myColorMap);
 
 
 
@@ -44,7 +48,7 @@ colormap(myColorMap);
 eavesKnowledge = zeros(numberOfSpots);
 
 for i = 1:numberOfSpots
-    eavesKnowledge(i,:) = RMWeights(secrecyCapacityPerLocation(i)+1);
+    eavesKnowledge(i,:) = RMWeights(ceil(secrecyCapacityPerLocation(i)+1));
 end
 
 [eavey eavex] = size(eavesKnowledge);
@@ -55,29 +59,40 @@ for i = 1:32
     yaxis(i,:) = i;
 end
 myColorMapScatter = jet(n+1);
+colormap(myColorMapScatter);
 
 
-
-for i = 1:eavex
-    
-    eave = scatter3(xaxis(i,:),yaxis(i,:),flipud(eavesKnowledge(i,:)),'s',...
-        'MarkerFaceColor',myColorMapScatter(eavesKnowledge(i,1)+1,:),...
-        'MarkerEdgeColor',myColorMapScatter(eavesKnowledge(i,1)+1,:));
-    
-    view(0,90);
-%     set(h, 'SizeData', markerWidth^2)
-    eave.SizeData = 100;
-    eave.MarkerFaceAlpha = .5;
-    eave.MarkerEdgeAlpha = .5;
-    
-end
+% for i = 1:eavex
+%     
+%     eave = scatter3(xaxis(i,:),yaxis(i,:),flipud(eavesKnowledge(i,:)),'s',...
+%         'MarkerFaceColor',myColorMapScatter(eavesKnowledge(i,1)+1,:),...
+%         'MarkerEdgeColor',myColorMapScatter(eavesKnowledge(i,1)+1,:));
+%   
+%     
+%     
+%     view(0,90);
+% %     set(h, 'SizeData', markerWidth^2)
+%     eave.SizeData = 100;
+%     eave.MarkerFaceAlpha = .5;
+%     eave.MarkerEdgeAlpha = .5;
+%     drawnow;
+% end
 % colormap(myColorMapScatter);
-colorbar;
-caxis([0 n]);
+% colorbar;
+% caxis([-3 n]);
+
+% eave = contourf(flipud(eavesKnowledge));
+% patches = findobj(eave,'-property','AlphaData');
+% for ph = patches
+%  set(ph,'AlphaData', 1 * get(ph,'AlphaData'));
+% end
+
+eave = pcolor(eavesKnowledge);
+set(eave,'EdgeAlpha',0);
 hold off;
 
-xlim([0 32]);
-ylim([0 32]);
+% xlim([0 32]);
+% ylim([0 32]);
 cf = gcf;
 cf.PaperSize = [5 4];
 cf.PaperPosition = [-.05521 0.2240 -0.5521+5 0.2240+4];
@@ -85,3 +100,26 @@ cf.PaperPosition = [-.05521 0.2240 -0.5521+5 0.2240+4];
 % %%
 % figure(100000)
 % imagesc(flipud(eavesKnowledge), 'AlphaData', .4)
+
+% close all
+bobPatch1 = patch([32 32 31 31],[1 32 32 1],'white');
+
+% Get patch objects from CONTOURGROUP
+bobHandle = findobj(bobPatch1, 'Type', 'patch');
+
+% Apply Hatch Fill
+bobHatch1 = hatchfill(bobHandle, 'single', 45, 5);
+
+% Remove outline
+set(bobPatch1, 'LineStyle', 'none');
+
+% Change the cross hatch to white
+set(bobHatch1, 'Color', 'black');
+
+xlim([0 32]);
+ylim([0 32]);
+hold off;
+
+
+%% 
+figure(10101010);
