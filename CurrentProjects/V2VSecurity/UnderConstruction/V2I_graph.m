b31342bb7fe077c57c54c4884218d88e83100da0
Benjamin Jensen <benjjensen@Live.com>
2019-03-06 18:19:10 -0700

    Updated V2I graph for better normalization

--- CurrentProjects/V2VSecurity/UnderConstruction/V2I_graph.m
+++ CurrentProjects/V2VSecurity/UnderConstruction/V2I_graph.m
@@ -6,29 +6,39 @@ data = graphPwelchedData;
 
     %% Calculate capacity
     
-capacity = zeros(numRows, 1);
+capacityPerCarrier = zeros(numRows, numCarriers);
+normCapacityPerCarrier = zeros(numRows, numCarriers);
+normCapacityPerLocation = zeros(numRows, 1);
+capacityPerLocation = zeros(numRows, 1);
 
     % Calculate the capacity per carrier, then sum up for location capacity
 for location = 1:numRows
-        temp = 0;
     for carrier = 1:numCarriers
-        temp = temp + 0.5*log2(1+data(location, carrier));
+        capacityPerCarrier(location, carrier) = 0.5*log2(1+data(location, carrier));
     end
-    capacity(location) = temp;
+end
+    
+    % Normalize 
+maxValue = max(max(capacityPerCarrier));
+normCapacityPerCarrier = capacityPerCarrier ./ maxValue;
+
+for location = 1:numRows
+    normCapacityPerLocation(location) = sum(normCapacityPerCarrier(location,:));
+    capacityPerLocation(location) = sum(capacityPerCarrier(location,:));
 end
 
-    % Normalize 
-maxValue = max(max(capacity));
-capacity = capacity ./ maxValue;
+
 
     % Plot in dB to see differences better
-capacity_dB = mag2db(capacity);
+capacity_dB = mag2db(capacityPerLocation);
+normCapacity_dB = mag2db(normCapacityPerLocation);
 
 figure()
 plot(capacity_dB);
-[maxCapacity, indexOfMaxCapacity] = max(capacity);  % Tx is at indexOfMaxCapacity (hopefully)
+% [maxCapacity, indexOfMaxCapacity] = max(capacity);  % Tx is at indexOfMaxCapacity (hopefully)
 
-    
+figure()
+plot(normCapacity_dB);
     %% Calculate secrecy capacity
     
 % capacity = capacity(1:10:end);
@@ -38,11 +48,11 @@ secrecyCapacity = zeros(numRows);
 
 for bobsLocation = 1:numRows
     for evesLocation = 1:numRows
-        if (capacity(bobsLocation) <= capacity(evesLocation))
+        if (normCapacityPerLocation(bobsLocation) <= normCapacityPerLocation(evesLocation))
             secrecyCapacity(bobsLocation, evesLocation) = 0;
         else 
             secrecyCapacity(bobsLocation, evesLocation) = ...
-                capacity(bobsLocation) - capacity(evesLocation);
+                normCapacityPerLocation(bobsLocation) - normCapacityPerLocation(evesLocation);
         end
     end
 end
@@ -55,7 +65,7 @@ xlabel("Bob's Location");
 ylabel("Eve's Location");
 xlim([0 4352]);
 ylim([0 4352]);
- zlim([-150 0]);
+zlim([-150 0]);
 view(45, 60);
 colormap(jet);
 
@@ -69,8 +79,5 @@ ylim([0 4352]);
 view(45, 60);
 colormap(jet);
 
-
-
-% 4) Repeat with "North" data
 
 
