a5ed5d58dee125550a3bf748d003e74ff06c8d9a
Bradford Clark <bradford96789@gmail.com>
2019-03-09 15:27:09 -0700

    i think these are finished

--- CurrentProjects/V2VSecurity/UnderConstruction/codedV2I.m
+++ CurrentProjects/V2VSecurity/UnderConstruction/codedV2I.m
@@ -12,113 +12,57 @@ load('SNRForV2I.mat');
 
 syms x;
 
-for k = 15:15
+for k = 30:30
+    
     maxNumberOfCarriers = 32;
     carriersPerLocation = cap_alpha(1:10:end,k);
     numberOfSpots = length(carriersPerLocation);
     
-    scaleEave = double(solve(435.2 == x * numberOfSpots,x));
+    %find scale values for eaves and bobs graphs
+    maxPosition = 435.2;
+    scaleEave = double(solve(maxPosition == x * numberOfSpots,x));
+    scaleBob = double(solve(maxPosition == x * numberOfSpots*2,x));
+    
+    %find RM weights 
     u = 2;
     m = 5;
-    
     kBits = 0;
     for i = 0 : u
         kBits = kBits + nchoosek(m,i);
     end
-    
     RMWeights = RMWeightHier(u,m,false);
-    
-    bobsKnowledge = zeros(numberOfSpots);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %set bobs knowledge to maxNumberOfCarriers only if he gets all carriers
+    bobsKnowledge = zeros(numberOfSpots*2);
     for i = 1:numberOfSpots
-        if carriersPerLocation(i) == 32
-            bobsKnowledge(:,2*i-1:2*i) = 0;
+        if carriersPerLocation(i) == maxNumberOfCarriers
+            bobsKnowledge(:,2*i-1:2*i) = maxNumberOfCarriers;
         else
             bobsKnowledge(:,2*i-1:2*i) = NaN;
         end
     end
-    figure(100+k);
-    hold on;
-    % bob = surf(flipud(bobsKnowledge));%-maxNumberOfCarriers);
-    % grid off;
-    % % cData = [1 1 1];
-    % caxis([0 32]);
-    % view(0,90);
-    % myColorMap = ones(128,3); % Set row 128 to white.
-    % % Then apply the colormap
-    % colormap(myColorMap);
     
-    
-    
-    %%
-    %%%%%% Find Eves matrix %%%%%%
-    eavesKnowledge = zeros(numberOfSpots);
+    %find Eves Equivocation matrix
+    eavesEquivocation = zeros(numberOfSpots);
     
     for i = 1:numberOfSpots
-        eavesKnowledge(i,:) = RMWeights(33-(ceil(carriersPerLocation(i))));
+        eavesEquivocation(i,:) = RMWeights(1+(carriersPerLocation(i)));
     end
     
-    [eavey eavex] = size(eavesKnowledge);
+    %create axis for graphs
+    [eavey eavex] = size(eavesEquivocation);
     xaxis = zeros(eavex);
     yaxis = zeros(eavey);
     for i = 1:32
         xaxis(:,i) = i;
         yaxis(i,:) = i;
     end
-    myColorMapScatter = jet(kBits+1);
-    colormap(myColorMapScatter);
     
+    %search for cases where bob gets all 32 bits
+    placesBobGetsAll = [];
+    placesBobGetsAll = strfind(bobsKnowledge(1,:),maxNumberOfCarriers)';
     
-    % for i = 1:eavex
-    %
-    %     eave = scatter3(xaxis(i,:),yaxis(i,:),flipud(eavesKnowledge(i,:)),'s',...
-    %         'MarkerFaceColor',myColorMapScatter(eavesKnowledge(i,1)+1,:),...
-    %         'MarkerEdgeColor',myColorMapScatter(eavesKnowledge(i,1)+1,:));
-    %
-    %
-    %
-    %     view(0,90);
-    % %     set(h, 'SizeData', markerWidth^2)
-    %     eave.SizeData = 100;
-    %     eave.MarkerFaceAlpha = .5;
-    %     eave.MarkerEdgeAlpha = .5;
-    %     drawnow;
-    % end
-    % colormap(myColorMapScatter);
-    % colorbar;
-    % caxis([-3 n]);
-    
-    % eave = contourf(flipud(eavesKnowledge));
-    % patches = findobj(eave,'-property','AlphaData');
-    % for ph = patches
-    %  set(ph,'AlphaData', 1 * get(ph,'AlphaData'));
-    % end
-    xyaxis = 0:scaleEave:eavex * scaleEave - scaleEave;
-    
-    eave = pcolor(xyaxis,xyaxis,eavesKnowledge);
-    set(eave,'EdgeAlpha',0);
-    colorBar = colorbar;
-    colorBar.Label.String = 'bits revealed to Eave';
-    colorBar.Label.FontSize = 15;
-    caxis([0 kBits]);
-    xlabel('Bobs Position');
-    ylabel('Eaves Position');
-    title('RM(1,5)');
-    % clabel('bits revealed to Eave');
-    hold off;
-    
-    % xlim([0 32]);
-    % ylim([0 32]);
-    cf = gcf;
-    cf.PaperSize = [5 4];
-    cf.PaperPosition = [-.05521 0.2240 -0.5521+5 0.2240+4];
-    
-    % %%
-    % figure(100000)
-    % imagesc(flipud(eavesKnowledge), 'AlphaData', .4)
-    
-    %%search for cases where bob gets all 32 bits
-    placesBobGetsAll = strfind(carriersPerLocation',maxNumberOfCarriers)';
+    %find vertices for the hatches
     countIndex = 1;
     placesBobGetsAllArea = [];
     for i = 1:length(placesBobGetsAll)
@@ -130,17 +74,34 @@ for k = 15:15
                 (placesBobGetsAll(i) - 1 == placesBobGetsAll(i-1)))
             placesBobGetsAllArea(countIndex) = placesBobGetsAll(i);
             countIndex = countIndex + 1;
-        elseif (i ~= 1 && i ~= length(placesBobGetsAll)) && xor((placesBobGetsAll(i-1) == placesBobGetsAll(i) - 1),(placesBobGetsAll(i+1) == placesBobGetsAll(i) + 1))
+        elseif (i ~= 1 && i ~= length(placesBobGetsAll)) &&...
+                xor((placesBobGetsAll(i-1) == placesBobGetsAll(i) - 1),...
+                (placesBobGetsAll(i+1) == placesBobGetsAll(i) + 1))
             placesBobGetsAllArea(countIndex) = placesBobGetsAll(i);
             countIndex = countIndex + 1;
         end
         
     end
     
+    %% plot Bobs and Eaves things
+    figure(100+k);
+    hold on;
+
+    %create colormap
+    myColorMapScatter = flipud(gray(kBits+1));
+    colormap(myColorMapScatter);
     
+    %find the xy axis
+    xyaxis = 0:scaleEave:eavex * scaleEave - scaleEave;
+    
+    %plot eaves equivocation
+    eave = pcolor(xyaxis,xyaxis,eavesEquivocation);
+    set(eave,'EdgeAlpha',0);
+
+    %create hatch marks for bob
     for i = 1:2:length(placesBobGetsAllArea)
-        firstSpot = placesBobGetsAllArea(i) * scaleEave;
-        secondSpot = placesBobGetsAllArea(i+1) * scaleEave;
+        firstSpot = placesBobGetsAllArea(i) * scaleBob;
+        secondSpot = placesBobGetsAllArea(i+1) * scaleBob;
         bobPatch1 = patch([firstSpot firstSpot secondSpot secondSpot],...
             [scaleEave eavex*scaleEave eavex*scaleEave scaleEave],'white');
         
@@ -154,14 +115,26 @@ for k = 15:15
         set(bobPatch1, 'LineStyle', 'none');
         
         % Change the cross hatch to white
-        set(bobHatch1, 'Color', 'white');
+        set(bobHatch1, 'Color', 'red');
         
     end
     
-    % xlim([1 32]);
-    % ylim([1 32]);
+    %set the looks for the graphs
+    caxis([0 kBits]);
+    colorBar = colorbar;
+    colorBar.Label.FontSize = 15;
+    colorBar.Label.String = 'Eaves bits of equivocation';
+    xlabel('Bobs Position (m)');
+    ylabel('Eaves Position (m)');
+    title(sprintf('Carriers Below %.2f dB are Erasures Coded With RM(%d,5)',10*log10(v2i_snr(k)),u));
+    xlim([0 maxPosition]);
+    ylim([0 maxPosition]);
+    
+    %sets the paper layout
+    cf = gcf;
+    cf.PaperSize = [5 4];
+    cf.PaperPosition = [-.05521 0.2240 -0.5521+5 0.2240+4];
+    
     hold off;
     drawnow;
-%     toc;
 end
-
