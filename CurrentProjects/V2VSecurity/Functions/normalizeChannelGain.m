eb85c2054b460cbe9fd94e2181c4144fc527dae2
Benjamin Jensen <benjjensen@Live.com>
2019-03-11 10:51:18 -0600

    Broke V2I graph into functions

---
+++ CurrentProjects/V2VSecurity/Functions/normalizeChannelGain.m
@@ -0,0 +1,11 @@
+function [normalizedGainSquared] = normalizeChannelGain(gSquared)
+%normalizeChannelGain Normalizes gSquared data by largest g
+%   Given the channel gain squared values (g^2) for pwelched measurements,
+%   this function solves for the largest channel gain (g), and then
+%   normalizes all the gains by that value. 
+    g = sqrt(gSquared);
+    maxValue = max(max(g));
+    normalizedG = g ./ maxValue;
+    normalizedGainSquared = normalizedG.^2;
+end
+
