1a38478e5d532bd6f870a99d0eef25b5636c52d8
Benjamin Jensen <benjjensen@Live.com>
2019-03-11 10:51:18 -0600

    Broke V2I graph into functions

--- CurrentProjects/V2VSecurity/UnderConstruction/V2I_graph.m
+++ CurrentProjects/V2VSecurity/UnderConstruction/V2I_graph.m
@@ -1,8 +1,16 @@
 clear; 
 close all;
+
+% load('../Functions');
+
 load('../Data/GraphPwelchedData/test-point-A_graph-pwelched-data.mat');
-data = graphPwelchedData;
-[numRows, numCarriers] = size(data);
+axisTicks = [-2000 -1000 0 1000 2000];
+
+% load('../Data/GraphPwelchedData/test-point-B_graph-pwleched-data.mat');
+% axisTicks = [-1500 -750 0 750 1500];
+
+gSquared = graphPwelchedData;
+[numRows, numCarriers] = size(gSquared);
 
     %% Calculate capacity  
     
@@ -12,14 +20,14 @@ capacityPerLocation = zeros(numRows, 1);
     % Calculate the capacity per carrier, then sum up for location capacity
     
 SNR = 1000;       % Arbitrary
-data = sqrt(data);
-maxValue = max(max(data));
-data = data ./ maxValue;
-data = data.^2;
+g = sqrt(gSquared);
+maxValue = max(max(g));
+normalizedG = g ./ maxValue;
+gSquared = normalizedG.^2;
 
 for location = 1:numRows
     for carrier = 1:numCarriers
-        capacityPerCarrier(location, carrier) = 0.5*log2(1+SNR*data(location, carrier));
+        capacityPerCarrier(location, carrier) = 0.5*log2(1+SNR*gSquared(location, carrier));
     end
 end
    
@@ -36,15 +44,10 @@ capacity_dB = mag2db(capacityPerLocation);
 
 [maxCapacity, indexOfMaxCapacity] = max(capacityPerLocation);  % Tx is at indexOfMaxCapacity (hopefully)
 
-% figure()
-% plot(capacity_dB);
-% xlabel("Location");
-% ylabel("Capacity (dB)");
-
 figure()
 plot(capacityPerLocation);
 xlabel("Location");
-ylabel("Capacity (linear)");
+ylabel("Capacity");
 
 
     %% Calculate secrecy capacity
@@ -69,36 +72,46 @@ end
 
 secrecyCapacity_dB = mag2db(secrecyCapacity);
 
+
 figure()
 h = surf(secrecyCapacity);
 set(h, 'LineStyle','none');     % Removes black lines so you can see the data
-ylabel("Bob's Location");
-xlabel("Eve's Location");
-zlabel("Secrecy Capacity (linear)");
-title("Secrecy Capacity, linear");
-xticks([indexOfMaxCapacity-2000 indexOfMaxCapacity-1000 indexOfMaxCapacity indexOfMaxCapacity+1000 indexOfMaxCapacity+2000]);
-xticklabels({'-2000', '-1000', '0', '1000', '2000'});
-yticks([indexOfMaxCapacity-2000 indexOfMaxCapacity-1000 indexOfMaxCapacity indexOfMaxCapacity+1000 indexOfMaxCapacity+2000]);
-yticklabels({'-2000', '-1000', '0', '1000', '2000'});
-xlim([0 4352]);
-ylim([0 4352]);
+ylabel("Bob's distance from Alice (m)");
+xlabel("Eve's distance from Alice (m)");
+zlabel("Secrecy Capacity (bits)");
+title("Secrecy Capacity");
+xticks([indexOfMaxCapacity+axisTicks(1) indexOfMaxCapacity+axisTicks(2) indexOfMaxCapacity+axisTicks(3) indexOfMaxCapacity+axisTicks(4) indexOfMaxCapacity+axisTicks(5)]);
+xticklabels({string(axisTicks(1)), string(axisTicks(2)), string(axisTicks(3)), string(axisTicks(4)), string(axisTicks(5))});
+yticks([indexOfMaxCapacity+axisTicks(1) indexOfMaxCapacity+axisTicks(2) indexOfMaxCapacity+axisTicks(3) indexOfMaxCapacity+axisTicks(4) indexOfMaxCapacity+axisTicks(5)]);
+yticklabels({string(axisTicks(1)), string(axisTicks(2)), string(axisTicks(3)), string(axisTicks(4)), string(axisTicks(5))});
+xlim([0 numRows]);
+ylim([0 numRows]);
 view(45, 60);
 colorbar;
 
-figure()
-contour(secrecyCapacity);
-ylabel("Bob's Location");
-xlabel("Eve's Location");
-title("Secrecy Capacity Contour, linear");
-xticks([indexOfMaxCapacity-2000 indexOfMaxCapacity-1000 indexOfMaxCapacity indexOfMaxCapacity+1000 indexOfMaxCapacity+2000]);
-xticklabels({'-2000', '-1000', '0', '1000', '2000'});
-yticks([indexOfMaxCapacity-2000 indexOfMaxCapacity-1000 indexOfMaxCapacity indexOfMaxCapacity+1000 indexOfMaxCapacity+2000]);
-yticklabels({'-2000', '-1000', '0', '1000', '2000'});
-xlim([0 4352]);
-ylim([0 4352]);
+
+
+% x = linspace(-2000, 2000, 4000);
+% y = x;
+% y1 = -x;
+
+hold on 
+imagesc(secrecyCapacity);
+plot(y,x,'LineWidth',2);
+plot(y1,x,'LineWidth',2);
+
+% figure()
+% contour(secrecyCapacity);
+ylabel("Bob's distance from Alice (m)");
+xlabel("Eve's distance from Alice (m)");
+title("Secrecy Capacity");
+xticks([indexOfMaxCapacity+axisTicks(1) indexOfMaxCapacity+axisTicks(2) indexOfMaxCapacity+axisTicks(3) indexOfMaxCapacity+axisTicks(4) indexOfMaxCapacity+axisTicks(5)]);
+xticklabels({string(axisTicks(1)/10), string(axisTicks(2)/10), string(axisTicks(3)/10), string(axisTicks(4)/10), string(axisTicks(5)/10)});
+yticks([indexOfMaxCapacity+axisTicks(1) indexOfMaxCapacity+axisTicks(2) indexOfMaxCapacity+axisTicks(3) indexOfMaxCapacity+axisTicks(4) indexOfMaxCapacity+axisTicks(5)]);
+yticklabels({string(axisTicks(1)/10), string(axisTicks(2)/10), string(axisTicks(3)/10), string(axisTicks(4)/10), string(axisTicks(5)/10)});
+xlim([0 numRows]);
+ylim([0 numRows]);
 colorbar;
 
-
-
-
+hold off
 
