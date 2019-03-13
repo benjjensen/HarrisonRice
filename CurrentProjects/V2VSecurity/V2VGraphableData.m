d3f8263278d2de9c3f2b0d8fa50cb2203ff84142
Nathan Nelson <nathan.nelson.97@gmail.com>
2019-03-13 15:46:50 -0600

    Added correct pwelching files

--- CurrentProjects/V2VSecurity/V2VGraphableData.m
+++ CurrentProjects/V2VSecurity/V2VGraphableData.m
@@ -1,3 +1,4 @@
+function V2VGraphableData(testPoint, stepSize, averageWidth)
 % V2VGraphableData
 % Takes the data from a V2V test point that is arranged with respect to
 % time and converts it to data that is arranged according to transmitter
@@ -5,43 +6,108 @@
 %
 % Author: Nathan Nelson
 
-addpath('Functions');
+    addpath('Functions');
 
-% The files where the data is stored. Change these if you want to do a
-% different test point.
-metadataFilename = 'Data/Metadata/test-point-J-V2V_2019-02-21__14-15-49-meta.txt';
-pwelchedDataFilename = 'Data/PwelchedData/test-point-J-V2V_pwelched-mid32.mat';
-outputFilename = 'Data/GraphPwelchedData/test-point-J_graph-pwelched-data.mat';
+    % The file where the Tx GPS data is stored.
+    txGPSFilename = 'Data/GPSData/Tx-gps-xcsv.txt';
 
-% The file where the Tx GPS data is stored.
-txGPSFilename = 'Data/GPSData/Tx-gps-xcsv.txt';
+    % The starting location, ending location, and step size.
+    % The starting and ending location were determined by looking at all of the
+    % data captures and determining where they overlap.
+    minD = 0;
+    maxD = 228.9;
+    deltaD = stepSize;
 
-% The starting location, ending location, and step size.
-% The starting and ending location were determined by looking at all of the
-% data captures and determining where they overlap. The step size was
-% chosen arbitrarily.
-minD = 0;
-maxD = 228.9;
-deltaD = 0.1;
+    % In order for this to work, we need to assign a particular location to be
+    % equal to zero. However, this location will probably be between our GPS
+    % data points; what we need to do for each test point is find the GPS data
+    % point that is closest to location zero and determine the location of that
+    % GPS data point. Then we'll move all of our GPS data points so that the
+    % correct data point is at the correct location.
 
-% In order for this to work, we need to assign a particular location to be
-% equal to zero. However, this location will probably be between our GPS
-% data points; what we need to do for each test point is find the GPS data
-% point that is closest to location zero and determine the location of that
-% GPS data point. Then we'll move all of our GPS data points so that the
-% correct data point is at the correct location.
-referenceIndex = 2;
-referenceDistance = 3.25;
+    if averageWidth == 1
+        allCarriersFilename = sprintf('Data/CorrectlyAveragedData/test-point-%s_jagged.mat', testPoint);
+        middle32Filename = sprintf('Data/CorrectlyAveragedData/test-point-%s_jagged-mid32.mat', testPoint);
+    else
+        allCarriersFilename = sprintf('Data/CorrectlyAveragedData/test-point-%s_averaged-%d.mat', testPoint, averageWidth);
+        middle32Filename = sprintf('Data/CorrectlyAveragedData/test-point-%s_averaged-%d-mid32.mat', testPoint, averageWidth);
+    end
+    
+    if testPoint == 'F'
+        metadataFilename = 'Data/Metadata/test-point-F-V2V_2019-02-21__13-59-52-meta.txt';
+        rawDataFilename = 'Data/PwelchedData/test-point-F-V2V_pwelched-mid32.mat';
+        
+        referenceIndex = 53;
+        referenceDistance = 2.46;
+    elseif testPoint == 'G'
+        metadataFilename = 'Data/Metadata/test-point-G-V2V_2019-02-21__14-04-26-meta.txt';
+        rawDataFilename = 'Data/PwelchedData/test-point-G-V2V_pwelched-mid32.mat';
+        
+        referenceIndex = 1;
+        referenceDistance = 0;
+    elseif testPoint == 'H'
+        metadataFilename = 'Data/Metadata/test-point-H-V2V_2019-02-21__14-07-35-meta.txt';
+        rawDataFilename = 'Data/PwelchedData/test-point-H-V2V_pwelched-mid32.mat';
+        
+        referenceIndex = 4;
+        referenceDistance = 0.08;
+    elseif testPoint == 'I'
+        metadataFilename = 'Data/Metadata/test-point-I-V2V_2019-02-21__14-10-46-meta.txt';
+        rawDataFilename = 'Data/PwelchedData/test-point-I-V2V_pwelched-mid32.mat';
+        
+        referenceIndex = 10;
+        referenceDistance = 6.23;
+    elseif testPoint == 'J'
+        metadataFilename = 'Data/Metadata/test-point-J-V2V_2019-02-21__14-15-49-meta.txt';
+        rawDataFilename = 'Data/PwelchedData/test-point-J-V2V_pwelched-mid32.mat';
+        
+        referenceIndex = 2;
+        referenceDistance = 3.25;
+    else
+        printf('Error: unkown test point %s', testPoint);
+        return;
+    end
+    
+    [RxDistances, blocks, startingTime] = ReadGPSDataPositions(metadataFilename);
+    TxDistances = ReadTxGPSLocations(txGPSFilename, startingTime, max(size(RxDistances)));
 
-[RxDistances, blocks, startingTime] = ReadGPSDataPositions(metadataFilename);
-TxDistances = ReadTxGPSLocations(txGPSFilename, startingTime, max(size(RxDistances)));
+    % Right now, TxDistances contain the distance that the transmitter has
+    % traveled from when we started recording data.
+    % Shift TxDistances so that it instead contains the distance that the
+    % transmitter has traveled from location 0.
+    TxDistances = TxDistances - TxDistances(referenceIndex) + referenceDistance;
+    
+    pwelchBoundaries = GetPwelchBoundaries(TxDistances, blocks, deltaD, minD, maxD);
+    if averageWidth ~= 1
+        pwelchBoundaries = GetAveragedPwelchBoundaries(pwelchBoundaries, averageWidth);
+    end
+    
+    PwelchOnFrameBoundaries(rawDataFilename, allCarriersFilename, ...
+        middle32Filename, pwelchBoundaries);
+end
 
-% Right now, TxDistances contain the distance that the transmitter has
-% traveled from when we started recording data.
-% Shift TxDistances so that it instead contains the distance that the
-% transmitter has traveled from location 0.
-TxDistances = TxDistances - TxDistances(referenceIndex) + referenceDistance;
-
-load(pwelchedDataFilename); % pwelched
-graphPwelchedData = GetDataPointsForGraph(TxDistances, blocks, pwelched, deltaD, minD, maxD);
-save(outputFilename, 'graphPwelchedData');
+function averagedPwelchBoundaries = GetAveragedPwelchBoundaries(boundaries, averageWidth)
+    averagedPwelchBoundaries = boundaries;
+    frameCount = max(size(boundaries));
+    for idx = 1:frameCount
+        startFrame = ceil(idx - (averageWidth-1)/2);
+        endFrame = ceil(idx + (averageWidth-1)/2);
+        startFrame = max(1, startFrame);
+        endFrame = min(frameCount, endFrame);
+        
+        startBoundary = boundaries(startFrame);
+        endBoundary = boundaries(endFrame);
+        
+        width = endBoundary - startBoundary + 1;
+        while mod(width,64)
+            if mod(width, 2)
+                endBoundary = endBoundary-1;
+            else
+                endBoundary = endBoundary-1;
+                startBoundary = startBoundary+1;
+            end
+            width = endBoundary - startBoundary + 1;
+        end
+        averagedPwelchBoundaries(idx, :) = [boundaries(startFrame) boundaries(endFrame)];
+    end
+end
