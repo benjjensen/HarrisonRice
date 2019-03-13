828bf7a3996b2d51935554bdc1ae729a0c132be7
Nathan Nelson <nathan.nelson.97@gmail.com>
2019-03-13 17:19:42 -0600

    Added script to generate graphable data for all data points.

--- CurrentProjects/V2VSecurity/V2VGraphableData.m
+++ CurrentProjects/V2VSecurity/V2VGraphableData.m
@@ -91,29 +91,3 @@ function V2VGraphableData(testPoint, stepSize, averageWidth)
     PwelchOnFrameBoundaries(rawDataFilename, allCarriersFilename, ...
         middle32Filename, pwelchBoundaries);
 end
-
-function averagedPwelchBoundaries = GetAveragedPwelchBoundaries(boundaries, averageWidth)
-    averagedPwelchBoundaries = boundaries;
-    frameCount = max(size(boundaries));
-    for idx = 1:frameCount
-        startFrame = ceil(idx - (averageWidth-1)/2);
-        endFrame = ceil(idx + (averageWidth-1)/2);
-        startFrame = max(1, startFrame);
-        endFrame = min(frameCount, endFrame);
-        
-        startBoundary = boundaries(startFrame);
-        endBoundary = boundaries(endFrame);
-        
-        width = endBoundary - startBoundary + 1;
-        while mod(width,64)
-            if mod(width, 2)
-                endBoundary = endBoundary-1;
-            else
-                endBoundary = endBoundary-1;
-                startBoundary = startBoundary+1;
-            end
-            width = endBoundary - startBoundary + 1;
-        end
-        averagedPwelchBoundaries(idx, :) = [boundaries(startFrame) boundaries(endFrame)];
-    end
-end
