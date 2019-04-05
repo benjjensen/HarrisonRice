9b2bca49e0563ab233e1f52560af6476e0586ff2
kalin <kalinnorman@gmail.com>
2019-04-05 10:29:46 -0600

    updated the test file

--- CurrentProjects/PSCJournal/RxTest.m
+++ CurrentProjects/PSCJournal/RxTest.m
@@ -17,43 +17,51 @@ for runs = 1:500
     ignore(:,runs) = rx();
 end
 
-test1begin = datetime;
-for runs = 1:frames
-    test(:,runs) = rx();
-end
-test1end = datetime;
-test1time = test1end - test1begin
-
-framesPerRun = 3500;
-numDivisions = frames/framesPerRun;
-split(numDivisions) = zeros;
-for init = 1:numDivisions
-    split(init) = init * framesPerRun;
-end
-test2 = zeros(2048,frames);
-timePerSection = duration('00:00:30');
-
-test2begin = datetime;
-for sections = 1:numDivisions
-    begintime = datetime;
-    for runs = split(sections)-(framesPerRun-1):split(sections)
-        test2(:,runs) = rx();
-    end
-    
-    while ((datetime - begintime) <= timePerSection)
-    end
-end
-test2end = datetime;
-test2time = test2end - test2begin
-
+% test1begin = datetime;
+% for runs = 1:frames
+%     test(:,runs) = rx();
+% end
+% test1end = datetime;
+% test1time = test1end - test1begin
+% 
+% framesPerRun = 3500;
+% numDivisions = frames/framesPerRun;
+% split(numDivisions) = zeros;
+% for init = 1:numDivisions
+%     split(init) = init * framesPerRun;
+% end
+% test2 = zeros(2048,frames);
+% timePerSection = duration('00:00:30');
+% 
+% test2begin = datetime;
+% for sections = 1:numDivisions
+%     begintime = datetime;
+%     for runs = split(sections)-(framesPerRun-1):split(sections)
+%         test2(:,runs) = rx();
+%     end
+%     
+%     while ((datetime - begintime) <= timePerSection)
+%     end
+% end
+% test2end = datetime;
+% test2time = test2end - test2begin
+%%
 for trials = 1:3
-test3length = duration('00:00:25');
-test3begin = datetime;
-count = 0;
-test3 = zeros(2048,1);
-while ((datetime - test3begin) < test3length)
-    test3(:,end+1) = rx();
-    count = count + 1;
+test3length = duration('00:00:03');
+test3 = zeros(2048,2000);
+% test3begin = datetime;
+tic;
+for temp = 1:400
+    test3(:,temp) = rx();
+%     pause();
 end
-count
+toc;
+% while ((datetime - test3begin) < test3length)
+%     test3(:,end+1) = rx();
+% end
+% size(test3)
 end
+
+
+
+
