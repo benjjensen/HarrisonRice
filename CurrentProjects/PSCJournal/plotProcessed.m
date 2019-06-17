e105ba76f68d306c09a7469fa1f7e158112be86b
Benjamin Jensen <benjjensen@Live.com>
2019-06-17 13:55:46 -0600

    Added in a limit-defined colorbar to plotProcessed.m

--- CurrentProjects/PSCJournal/plotProcessed.m
+++ CurrentProjects/PSCJournal/plotProcessed.m
@@ -4,16 +4,22 @@ load SPECIALFOLDER/processed.mat;
 figure();
 imagesc(harrisonComplete);
 ax1 = gca; title('Harrison');
+colorbar;
+caxis([0 500]);
 ax1.YDir = 'normal'; drawnow;
 
 figure();
 imagesc(smalleyComplete);
 ax2 = gca; title('Smalley');
+colorbar;
+caxis([0 500]);
 ax2.YDir = 'normal'; drawnow;
 
 figure();
 imagesc(camachoComplete);
 ax3 = gca; title('Camacho');
+colorbar;
+caxis([0 500]);
 ax3.YDir = 'normal'; drawnow;
 
 hmax = max(max(harrisonComplete));
