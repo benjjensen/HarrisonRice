e02a6a21230593ab8b550ddd73ff8c3493d367d7
Benjamin Jensen <benjjensen@Live.com>
2019-07-19 14:17:51 -0600

    Added in starting and ending carriers

--- CurrentProjects/PSCJournal/CompareGoodCarriers.m
+++ CurrentProjects/PSCJournal/CompareGoodCarriers.m
@@ -63,9 +63,9 @@ smaSignal = smaSignal ./ trueMax;
 
     % Count number of carriers above a threshold for a range of thresholds
 for SNR = 1:100
-    counts(1, SNR) = countGoodCarriers(harSignal, aveNoiseHar, SNR);
-    counts(2, SNR) = countGoodCarriers(smaSignal, aveNoiseSma, SNR);
-    counts(3, SNR) = countGoodCarriers(camSignal, aveNoiseCam, SNR);
+    counts(1, SNR) = countGoodCarriers(harSignal, aveNoiseHar, SNR, startCarrier, endCarrier);
+    counts(2, SNR) = countGoodCarriers(smaSignal, aveNoiseSma, SNR, startCarrier, endCarrier);
+    counts(3, SNR) = countGoodCarriers(camSignal, aveNoiseCam, SNR, startCarrier, endCarrier);
 end
 
 difference = min((counts(1,:) - counts(2,:)),(counts(1,:) - counts(3,:)));
@@ -73,7 +73,7 @@ difference = min((counts(1,:) - counts(2,:)),(counts(1,:) - counts(3,:)));
 %%%%%%%%%%%%%%%%%%%%
 %%%%%%% PLOT %%%%%%%
 %%%%%%%%%%%%%%%%%%%%
-x
+
 figure()
 hold on
 plot(counts(1,:), 'k');
--- CurrentProjects/PSCJournal/Functions/countGoodCarriers.m
+++ CurrentProjects/PSCJournal/Functions/countGoodCarriers.m
@@ -1,4 +1,4 @@
-function [aveNumGoodCarriers] = countGoodCarriers(signal, noise, threshold)
+function [aveNumGoodCarriers] = countGoodCarriers(signal, noise, threshold, startCar, endCar)
 
     [numCarriers, numSamples] = size(signal);
     numGoodCarriers = zeros(numSamples, 1);
@@ -8,7 +8,7 @@ function [aveNumGoodCarriers] = countGoodCarriers(signal, noise, threshold)
     
     for sample = 1:numSamples
         numGood = 0;
-        for carrier = 1:numCarriers
+        for carrier = startCar:endCar
             if ((signalDb(carrier, sample) - noiseDb(1, sample))  >= threshold)
                 numGood = numGood + 1;
             end
