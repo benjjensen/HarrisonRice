ddb813f9e7e09a510000cbb067b6be59004daae6
kalin <kalinnorman@gmail.com>
2019-07-23 09:23:31 -0600

    Removed old V2V files to then be replaced with the new ones

--- GeneralV2VCode/V2V/matlab/readV2VData.m
+++
@@ -1,9 +0,0 @@
-function data = readV2VData(filename, offset, samples)
-    data_file_id = fopen(filename);
-    
-    fseek(data_file_id, offset, 'bof');
-    
-    data = fread(data_file_id, samples, 'unsigned short');
-    
-    fclose(data_file_id);
-end
