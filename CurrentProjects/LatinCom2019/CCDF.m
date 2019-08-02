8841bc2f56a4cb97c59d09eccdbbdc01cc1f6cfb
Benjamin Jensen <benjjensen@Live.com>
2019-08-02 11:51:00 -0600

    Updated CCDF

--- CurrentProjects/LatinCom2019/CCDF.m
+++ CurrentProjects/LatinCom2019/CCDF.m
@@ -41,10 +41,11 @@ grid on
 figure()
 semilogy(xaxis(1:end-1), 1-cumulative(1:end-1)); grid on;
 % plot(1-cummulative); grid on;
-title('CCDF (semilogy)');
+% title('CCDF (semilogy)');
 xlabel('Power Received');
 ylabel('Probability');
 grid on
+saveas(gcf,'Figures/CCDF','epsc');
 
 % Normalized it so that 0 dB power is when the vans are right next to one
 % another. (dB relative to when they are side by side)
