937341aa956b7a7faa569bc3d00fc89df2bdf52a
kalin <kalinnorman@gmail.com>
2019-09-12 13:53:20 -0600

    Modified the receiver file for the MARB data collection

--- CurrentProjects/MARB/MarbReceiver.m
+++ CurrentProjects/MARB/MarbReceiver.m
@@ -2,43 +2,67 @@
 % Pulls in and saves data from an SDR
 % (Sets up the radio, accounts for the buffer error)
 
-clear; close all;
+clearvars -except rx; close all;
 warning('off','all')   % Gets rid of the super annoying version warnings
 
 %%% USER INFORMATION %%%%%%
-frames = 1500; % approximately 1500 frames per minute, depending on the computer
-nameOfArray = "Test";
+framesPerMinute = 7500; % approximately 4250 frames per minute, depending on the computer
+framesPerSection = 500;
+minutesOfData = 4;
+pauseTime = 0.0;
+nameOfArray = 'test';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % Radio setup
-plutoradiosetup();
-rx = sdrrx('Pluto');
-rx.RadioID = 'usb:0';
-rx.CenterFrequency = 1250e6;
-rx.BasebandSampleRate = 20e6;
-rx.SamplesPerFrame = 32*64;
-rx.OutputDataType = 'double';
-rx.ShowAdvancedProperties = true;
+DurationForRadioSetup = duration('00:0:05');
+RadioSetupBeginTime = datetime;
 
+if ~exist('rx','var')
+        % Radio setup
+    plutoradiosetup();
+    rx = sdrrx('Pluto');
+    rx.RadioID = 'usb:0';
+    rx.CenterFrequency = 1250e6;
+    rx.BasebandSampleRate = 20e6;
+    rx.SamplesPerFrame = 32*64;
+    rx.OutputDataType = 'double';
+    rx.ShowAdvancedProperties = true;
+end
+    % Initial pull from the radio (helps sync timing)?
+for ignoreme = 1:50
+    dostuff = rx();
+end
 
-    % Account for the buffer
-ignore = zeros(2048,1);
-temp = zeros(2048, frames);
-for init = 1:100
-    ignore(:,1) = rx();
+    % Initialize Variables
+totalFrames = framesPerMinute * minutesOfData;
+numSections = totalFrames / framesPerSection;
+firstIndexOfSection = 1;
+lastIndexOfSection = framesPerSection;
+timePerSection = duration('00:0:04');
+temp = zeros(2048, totalFrames);
+
+    % Wait for the setup duration specified above
+while((datetime - RadioSetupBeginTime) < DurationForRadioSetup)
 end
 
     % Gather Data
-for count = 1:frames
-    temp(:, count) = rx();
-end 
+DataCollectionStartTime = datetime;
+for timingSections = 1:numSections
+    SectionStartTime = datetime;
+    for index = firstIndexOfSection:lastIndexOfSection
+        temp(:,index) = rx();
+        pause(pauseTime); % for the computers that run too fast...
+    end
+    firstIndexOfSection = firstIndexOfSection + framesPerSection;
+    lastIndexOfSection = lastIndexOfSection + framesPerSection;
+    while ((datetime - SectionStartTime) <= timePerSection)
+    end
+end
+DataCollectionFinishTime = datetime;
+DataCollectionRunTime = DataCollectionFinishTime - DataCollectionStartTime;
 
     % Save the file
 eval(sprintf("%s = temp;",string(nameOfArray)));
 i = string(nameOfArray); % name of data array
 save(string(i) + '.mat',string(i)); % Saves data array with custom name
 
-beep;
-
-
-
+disp('Program Finished. Runtime was: ' + string(DataCollectionRunTime) + '. Results saved as ' + string(nameOfArray) + '.mat');
